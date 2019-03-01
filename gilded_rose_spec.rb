d99ee7f9af272fd95ce7235f97b1fb409a2ec79f
Jeremy Shearer <jeremy.shearer@daveramsey.com>
2019-03-01 11:16:54 -0600

    Add tests for brie and backstage passes

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,55 +1,149 @@
 # frozen_string_literal: true
-
+require "ostruct"
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
-describe GildedRose do
+RSpec.describe GildedRose do
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].name).to eq 'foo'
-    end
-
     it 'decreases the sellIn of an item' do
       items = [create_item(sell_in: 5)]
+
       GildedRose.new(items).update_quality
+
       expect(items[0].sell_in).to eq 4
     end
 
     it 'decreases the quality of an item as it ages' do
-      items = [create_item(sell_in: 5, quality: 5)]
+      items = [create_item(quality: 5)]
+
       GildedRose.new(items).update_quality
+
       expect(items[0].quality).to eq 4
     end
 
     it 'decreases the quality by 1 when sellIn is 1' do
-      items = [Item.new('foo', 1, 5)]
+      items = [create_item(sell_in: 1, quality: 5)]
+
       GildedRose.new(items).update_quality
+
       expect(items[0].quality).to eq 4
     end
 
     it 'decreases the quality twice as fast when sellIn is 0' do
-      items = [Item.new('foo', 0, 5)]
+      items = [create_item(sell_in: 0, quality: 5)]
+
       GildedRose.new(items).update_quality
+
       expect(items[0].quality).to eq 3
     end
 
     it 'decreases the quality twice as fast when sellIn is -1' do
-      items = [Item.new('foo', -1, 5)]
+      items = [create_item(sell_in: -1, quality: 5)]
+
       GildedRose.new(items).update_quality
+
       expect(items[0].quality).to eq 3
     end
 
-    it 'does not decrease in quality when the item is sulfruas' do
-      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 80)]
+    it 'decreases the quality twice as fast when sellIn is really old' do
+      items = [create_item(sell_in: -5, quality: 5)]
+
       GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 80
+
+      expect(items[0].quality).to eq 3
     end
 
-    it 'increases the quality of aged brie as sellIn decreases' do
-      items = [Item.new("Aged Brie", 2, 0)]
+    it 'does not allow the quality to be a negative value' do
+      items = [create_item(sell_in: 5, quality: 0)]
+
       GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 1
+
+      expect(items[0].quality).to eq 0
+    end
+
+    it 'does not allow the quality to be a negative value' do
+      items = [create_item(sell_in: -1, quality: 0)]
+
+      GildedRose.new(items).update_quality
+
+      expect(items[0].quality).to eq 0
+    end
+
+
+    it 'does not decrease in quality when the item is Sulfuras' do
+      items = [create_item(name: 'Sulfuras, Hand of Ragnaros', sell_in: 0, quality: 80)]
+
+      GildedRose.new(items).update_quality
+
+      expect(items[0].quality).to eq 80
+      expect(items[0].sell_in).to eq 0
+    end
+
+    context 'when the item name is "Aged Brie"' do
+      it 'increases the quality as sell_in decreases' do
+        items = [Item.new("Aged Brie", 2, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 1
+      end
+
+      it 'does not increase the quality above 50' do
+        items = [Item.new("Aged Brie", 2, 50)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 50
+      end
+
+      it 'increases the quality by 2 when sell_in is negative' do
+        items = [Item.new("Aged Brie", -1, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 2
+      end
+    end
+
+    context 'when the item is "Backstage passes"' do
+      it 'increases the quality by 1 when sell_in is 11' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 1
+      end
+
+      it 'increases the quality by 2 when sell_in is 10' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'increases the quality by 2 when sell_in is 8' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 8, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'increases the quality by 2 when sell_in is 6' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'increases the quality by 3 when sell_in is 5' do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+
+        GildedRose.new(items).update_quality
+
+        expect(items[0].quality).to eq 3
+      end
     end
   end
 end
